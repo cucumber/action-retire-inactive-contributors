9c316170e10c985d7d5108a907680921298dcaf7
Matt Wynne <matt@cucumber.io>
2022-02-25 09:38:07 -0800

    Call the bot from our scenario.
    We used the "issues.opened" event for now, just because that's what was in the Probot example code, but that's probably not the event we'll want to use in the end.
    
    Co-authored-by: Kate Dames <funficient@users.noreply.github.com>
    Co-authored-by: Mona <BlueMona@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import { Probot } from "probot";
+import { Probot } from "probot"
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
+  app.on("issues.opened", async (_context) => {
+    console.log("Hello!")
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
+}
