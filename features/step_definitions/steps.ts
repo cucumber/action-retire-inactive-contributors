aa1008f532de6fe03207d68391cb1512d6661a9b
Matt Wynne <matt@cucumber.io>
2022-04-15 09:22:36 -0700

    Started to design the interface for our GitHub port
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <funficient@users.noreply.github.com>

--- features/step_definitions/steps.ts
+++ features/step_definitions/steps.ts
@@ -1,5 +1,44 @@
-import { Given, When, Then, defineParameterType } from "@cucumber/cucumber"
+import {
+  Before,
+  Given,
+  When,
+  Then,
+  defineParameterType,
+} from "@cucumber/cucumber"
+import assert from "assert"
 import { retireInactiveContributors } from "../../src/retireInactiveContributors"
+
+type World = { github: FakeGitHub }
+
+class FakeGitHub {
+  getMembersOf(team: string): string[] {
+    console.log("TODO: Get members of ", team)
+    return ["someuser", "anotheruser"]
+  }
+  createOrg(organization: string) {
+    console.log("TODO: create an org named", organization)
+  }
+
+  createUser(user: string) {
+    console.log("TODO: ensure a user exists named", user)
+  }
+
+  createTeam(team: string) {
+    console.log("TODO: ensure a team exsits named", team)
+  }
+
+  addUserToTeam(user: string, team: string) {
+    console.log("TODO: add user to", team)
+  }
+
+  createCommit(daysAgo: number) {
+    console.log("TODO: create a commit", daysAgo, "days ago")
+  }
+}
+
+Before(function () {
+  this.github = new FakeGitHub()
+})
 
 defineParameterType({
   regexp: /the ([\w-]+) team/,
@@ -13,9 +52,12 @@ defineParameterType({
   name: "user",
 })
 
-Given("a GitHub organization {string}", function (organization: string) {
-  console.log("TODO: create an org named", organization)
-})
+Given(
+  "a GitHub organization {string}",
+  function (this: World, organization: string) {
+    this.github.createOrg(organization)
+  }
+)
 
 Given(
   "a user {user} has write access to the cucumber-js repo",
@@ -28,19 +70,17 @@ Given(
 
 Given(
   "a user {user} is part/member of {team}",
-  function (user: string, team: string) {
-    console.log("TODO: ensure a user exists named", user)
-    console.log("TODO: ensure a team exsits named", team)
-    console.log("TODO: add user to", team)
+  function (this: World, user: string, team: string) {
+    this.github.createUser(user)
+    this.github.createTeam(team)
+    this.github.addUserToTeam(user, team)
   }
 )
 
 Given(
   "the create date of their last commit was {int} day/days ago",
-  function (daysAgo: number) {
-    // Given('the create date of their last commit was {float} day ago', function (float) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("TODO: create a commit", daysAgo, "days ago")
+  function (this: World, daysAgo: number) {
+    this.github.createCommit(daysAgo)
   }
 )
 
@@ -59,9 +99,16 @@ When("the job is scheduled to run on the organization", function () {
   retireInactiveContributors()
 })
 
-Then("{user} should still be in {team}", function (user: string, team: string) {
-  console.log("TODO: assert that", user, "is indeed is in", team)
-})
+Then(
+  "{user} should still be in {team}",
+  function (this: World, user: string, team: string) {
+    const users = this.github.getMembersOf(team)
+    assert(
+      users.includes(user),
+      `Could not find user: ${user} in team: ${team}. Users found: ${users}`
+    )
+  }
+)
 
 Then("{user} should be in {team}", function (user: string, team: string) {
   // "Writede here that turns the phrase above into concrete actions
