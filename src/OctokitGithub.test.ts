992fd0e9a319ec19be2c11045f5ea08cf4661f2c
Matt Wynne <matt@cucumber.io>
2022-05-25 11:54:31 -0700

    Call the getMembersOf method from the unit test
    Co-authored-by: marjuhirsh <marjuhirsh@users.noreply.github.com>
    Co-authored-by: Arti M <artismarti@users.noreply.github.com>
    Co-authored-by: Meena <aneemqah@users.noreply.github.com>

--- src/OctokitGithub.test.ts
+++ src/OctokitGithub.test.ts
@@ -2,7 +2,8 @@ import assert from 'assert'
 import { OctokitGithub } from './OctokitGithub'
 
 describe(OctokitGithub.name, () => {
-  it('works', () => {
-    assert(false)
+  it('gets members of a team', async () => {
+    const github = new OctokitGithub()
+    github.getMembersOf('fishcakes')
   })
 })
