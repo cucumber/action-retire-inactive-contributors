45fefd0743422704d5b7847f74d9a53148269088
blaisep <blaise@gmail.com>
2022-06-01 15:42:28 -0400

    WIP: next step is to get the auth token figured out because we get: ``` 1) OctokitGithub        gets members of a team:      Error: Parameter token or opts.auth is required       at Object.getAuthString (node_modules/@actions/github/src/internal/utils.ts:10:11)       at Object.getOctokitOptions (node_modules/@actions/github/src/utils.ts:38:22)       at getOctokit (node_modules/@actions/github/src/github.ts:19:21)       at /Users/bpabon/src/inactive-contributor-bot/src/OctokitGithub.test.ts:7:31       at Generator.next (<anonymous>)       at /Users/bpabon/src/inactive-contributor-bot/src/OctokitGithub.test.ts:8:71       at new Promise (<anonymous>)       at __awaiter (src/OctokitGithub.test.ts:4:12)       at Context.<anonymous> (src/OctokitGithub.test.ts:6:43)       at processImmediate (node:internal/timers:466:21)       ```       Co-authored-by: Marju Hirsh <marjutubli@gmail.com>

--- src/OctokitGithub.test.ts
+++ src/OctokitGithub.test.ts
@@ -4,7 +4,7 @@ import { getOctokit } from '@actions/github'
 
 describe(OctokitGithub.name, () => {
   it('gets members of a team', async () => {
-    const octokit = getOctokit('github-token')
+    const octokit = getOctokit('')
     const githubAdapter = new OctokitGithub(octokit)
     const members = githubAdapter.getMembersOf('fishcakes')
     assert(members)
