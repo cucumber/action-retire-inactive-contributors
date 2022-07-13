2a3d19bfb15b0ae2138487b08c65a705615946ab
Matt Wynne <matt@cucumber.io>
2022-07-13 12:51:17 -0700

    Run npm build as part of npm test (#17)

--- dist/retireInactiveContributors.js
+++ dist/retireInactiveContributors.js
@@ -2,14 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.retireInactiveContributors = void 0;
 function retireInactiveContributors(github) {
-    // return alumni team, a member called Greg and the last commit date
-    const alumniTeam = "alumni";
-    const committersTeam = "committers";
+    const alumniTeam = 'alumni';
+    const committersTeam = 'committers';
     const committersTeamMembers = github.getMembersOf(committersTeam);
     for (const user of committersTeamMembers) {
         const lastCommit = github.getLastCommitBy(user);
         const oneDay = 1000 * 60 * 60 * 24;
-        const daysSinceLastCommit = Math.round(new Date().getTime() - lastCommit.date.getTime() / oneDay);
+        const daysSinceLastCommit = Math.round((new Date().getTime() - lastCommit.date.getTime()) / oneDay);
         if (daysSinceLastCommit >= 365) {
             github.addUserToTeam(user, alumniTeam);
             github.removeUserFromTeam(user, committersTeam);
