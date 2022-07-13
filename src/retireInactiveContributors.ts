7fed862ee619e4d8e7280766eec7cdea698153ef
Matt Wynne <matt@cucumber.io>
2022-07-13 12:49:11 -0700

    WIP

--- src/retireInactiveContributors.ts
+++ src/retireInactiveContributors.ts
@@ -4,18 +4,20 @@ export type Commit = {
 }
 
 export interface Github {
-  removeUserFromTeam(user: string, committersTeam: string): void
-  getLastCommitBy(user: string): Commit
-  addUserToTeam(user: string, alumniTeam: string): void
-  getMembersOf(team: string): string[]
+  removeUserFromTeam(user: string, committersTeam: string): Promise<void>
+  getLastCommitBy(user: string): Promise<Commit>
+  addUserToTeam(user: string, alumniTeam: string): Promise<void>
+  getMembersOf(team: string): Promise<string[]>
 }
 
-export function retireInactiveContributors(github: Github): void {
+export async function retireInactiveContributors(
+  github: Github
+): Promise<void> {
   const alumniTeam = 'alumni'
   const committersTeam = 'committers'
-  const committersTeamMembers = github.getMembersOf(committersTeam)
+  const committersTeamMembers = await github.getMembersOf(committersTeam)
   for (const user of committersTeamMembers) {
-    const lastCommit = github.getLastCommitBy(user)
+    const lastCommit = await github.getLastCommitBy(user)
     const oneDay = 1000 * 60 * 60 * 24
     const daysSinceLastCommit = Math.round(
       (new Date().getTime() - lastCommit.date.getTime()) / oneDay
