449cd89c1d598cb8231427351b7d0e2b2ae11c30
Matt Wynne <matt@cucumber.io>
2022-07-20 13:55:33 -0500

    Promisify GitHub api (#18)
    * WIP
    
    * Include feature files in `npm run build`
    
    * Configure separate tsc builds for dev and production
    
    * Finish async changes
    
    * Clearly distinguish between dev and production builds
    
    * More consistent tsconfig options with previous

--- src/OctokitGithub.ts
+++ src/OctokitGithub.ts
@@ -2,19 +2,19 @@ import { Commit, Github } from './retireInactiveContributors'
 // "TODO: Implement this using actual API calls to GitHub via Octokit library"
 
 export class OctokitGithub implements Github {
-  getLastCommitBy(user: string): Commit {
+  getLastCommitBy(user: string): Promise<Commit> {
     throw new Error('Function not implemented.')
   }
 
-  addUserToTeam(user: string, alumniTeam: string): void {
+  addUserToTeam(user: string, alumniTeam: string): Promise<void> {
     throw new Error('Function not implemented.')
   }
 
-  removeUserFromTeam(user: string, committersTeam: string): void {
+  removeUserFromTeam(user: string, committersTeam: string): Promise<void> {
     throw new Error('Function not implemented.')
   }
 
-  getMembersOf(team: string): string[] {
+  getMembersOf(team: string): Promise<string[]> {
     throw new Error('Function not implemented.')
   }
 }
