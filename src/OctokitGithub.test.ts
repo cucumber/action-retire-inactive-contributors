a0e923d95884e54b7d7204e9ce2b6d136ed4210d
Matt Wynne <matt@cucumber.io>
2022-07-27 12:18:04 -0700

    Implement truthy test case for hasCommittedSince
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    Co-authored-by: Olle Jonsson <olle.jonsson@gmail.com>

--- src/OctokitGithub.test.ts
+++ src/OctokitGithub.test.ts
@@ -22,8 +22,26 @@ describe(OctokitGithub.name, () => {
       )
       assertThat(hasCommitted, is(falsey()))
     })
-    // TODO: implement this test!
-    it('returns true if they have')
+
+    it('returns true if they have', async () => {
+      const token = process.env.GITHUB_TOKEN
+      if (!token) {
+        throw new Error(
+          'Please set GITHUB_TOKEN. See https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
+        )
+      }
+      const octokit = getOctokit(token)
+      const githubAdapter = new OctokitGithub(
+        octokit,
+        'test-inactive-contributor-action'
+      )
+      const dateOnWhichMattCommitted = new Date(2022, 3, 1) // April 1.
+      const hasCommitted = await githubAdapter.hasCommittedSince(
+        'mattwynne',
+        dateOnWhichMattCommitted
+      )
+      assertThat(hasCommitted, is(true))
+    })
   })
 
   it('gets members of a team', async () => {
