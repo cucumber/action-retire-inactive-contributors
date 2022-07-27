ec5441ea81fc3a1c3365fb3d3e69fdc34e0d72e0
Matt Wynne <matt@cucumber.io>
2022-07-27 11:31:53 -0700

    Add failing test for getLastCommitBy
    Co-authored-by: Blaise Pabon <blaise@gmail.com>
    Co-authored-by: Kate Dames <kate.funficient@gmail.com>
    Co-authored-by: Olle Jonsson <olle.jonsson@gmail.com>

--- src/OctokitGithub.test.ts
+++ src/OctokitGithub.test.ts
@@ -1,8 +1,25 @@
 import { OctokitGithub } from './OctokitGithub'
 import { getOctokit } from '@actions/github'
-import { assertThat, equalTo } from 'hamjest'
+import { assertThat, defined, equalTo } from 'hamjest'
 
 describe(OctokitGithub.name, () => {
+  it('Get the last commit by github user name.', async () => {
+    const token = process.env.GITHUB_TOKEN
+    if (!token) {
+      throw new Error(
+        'Please set GITHUB_TOKEN. See https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
+      )
+    }
+    const octokit = getOctokit(token)
+    const githubAdapter = new OctokitGithub(
+      octokit,
+      'test-inactive-contributor-action'
+    )
+    const commit = await githubAdapter.getLastCommitBy('mattwynne')
+    console.log(commit)
+    assertThat(commit, defined())
+  })
+
   it('gets members of a team', async () => {
     const token = process.env.GITHUB_TOKEN
     if (!token) {
