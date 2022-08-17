fa2cf3d9c5b42f64617680dd3d0ce08a232117f6
Matt Wynne <matt@cucumber.io>
2022-08-17 11:43:24 -0700

    Tidy up TODOs
    Co-authored-by: Blaise Pabon <blaise@gmail.com>

--- src/OctokitGithub.ts
+++ src/OctokitGithub.ts
@@ -1,6 +1,5 @@
 import { GitHub } from '@actions/github/lib/utils'
 import { GitHubClient } from './retireInactiveContributors'
-// "TODO: Implement this using actual API calls to GitHub via Octokit library"
 
 export class OctokitGitHub implements GitHubClient {
   constructor(
@@ -26,20 +25,20 @@ export class OctokitGitHub implements GitHubClient {
   }
 
   addUserToTeam(user: string, alumniTeam: string): Promise<void> {
+    // "TODO: Implement this using actual API calls to GitHub via Octokit library"
     throw new Error('Function not implemented.')
   }
 
   removeUserFromTeam(user: string, committersTeam: string): Promise<void> {
+    // "TODO: Implement this using actual API calls to GitHub via Octokit library"
     throw new Error('Function not implemented.')
   }
 
   async getMembersOf(team: string): Promise<string[]> {
-    // TODO: change interface to make these functions async (i.e. they need to return Promises)
     const result = await this.octokit.rest.teams.listMembersInOrg({
       org: this.org,
       team_slug: team,
     })
-
     return result.data.map((user) => user.login)
   }
 }
