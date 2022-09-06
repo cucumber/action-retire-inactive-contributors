be8331615c4c5be209f189ad0eefef28dc2bf54e
Matt Wynne <matt@cucumber.io>
2022-09-06 14:41:37 -0700

    Add Cucumber Report to CI

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,6 @@ module.exports = {
     default: [
       "--require-module ts-node/register",
       "--require features/**/*.ts",
-      "--publish-quiet",
+      "--publish"
     ].join(" "),
   };
