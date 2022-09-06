3c0098f1ceea7ab4b34e0ebc2491ffe686ef901b
Matt Wynne <matt@cucumber.io>
2022-09-06 14:46:20 -0700

    Only push if the token environment variable is set
    Otherwise, stay quiet

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,8 @@
 module.exports = {
-    default: [
-      "--require-module ts-node/register",
-      "--require features/**/*.ts",
-      "--publish"
-    ].join(" "),
+  default: [
+      '--require-module ts-node/register', 
+      '--require features/**/*.ts'
+    ]
+    .concat(process.env.CUCUMBER_PUBLISH_TOKEN ? "--publish" : "--publish-quiet")
+    .join(" "),
   };
