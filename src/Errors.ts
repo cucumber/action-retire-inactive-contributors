556768ea37cf406f13bf2cc8c6b4e985d1270183
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2024-10-25 00:02:07 +0700

    move error related types and functions to another file

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,1 +1,25 @@
-export class UnableToGetMembersError extends Error {}
+class UnableToGetMembersError extends Error {}
+
+class UnableToGetRepositoriesError extends Error {}
+
+type GithubRequestError = {
+    message: string
+    request: {
+      url: string
+    }
+}
+
+function isGithubRequestError(
+    candidate: unknown
+    ): candidate is GithubRequestError {
+    return Boolean(
+        candidate && typeof candidate == 'object' && 'request' in candidate
+    )
+}
+
+export {
+    UnableToGetMembersError,
+    UnableToGetRepositoriesError,
+    GithubRequestError,
+    isGithubRequestError
+}
